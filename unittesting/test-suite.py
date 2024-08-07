d0f71a6917ba12d40882b57e24fae7dafaa64f57
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-08-07 22:05:48 +0545

    test suite

--- unittesting/test-suite.py
+++ unittesting/test-suite.py
@@ -1,4 +1,27 @@
 import unittest
 
 class suiteTest(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.a = 10
+        self.b = 20
+        
+    def testadd(self):
+        """Add"""
+        result = self.a + self.b 
+        self.assertTrue(result == 100)
+
+    def testsub(self):
+        """sub"""
+        result = self.a - self.b 
+        
+def suite():
+    suite = unittest.TestSuite()
+    # suite.addTest (simpleTest3("testadd"))
+    ## suite.addTest (simpleTest3("testsub"))
+    suite.addTest(unittest.makeSuite(simpleTest3))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    test_suite = suite()
+    runner.run (test_suite)
