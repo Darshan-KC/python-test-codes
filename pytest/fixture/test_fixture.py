fa26650f21a34a925737a195fa2f1e861f50db97
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-08-13 20:36:35 +0545

    test_fixture complete

--- pytest/fixture/test_fixture.py
+++ pytest/fixture/test_fixture.py
@@ -4,3 +4,13 @@ import pytest
 def sample_data():
     data = [1,2,3,4,5]
     return data
+
+def test_sum(sample_data):
+    assert sum(sample_data) == 15
+
+def test_len(sample_data):
+    assert len(sample_data) == 5
+
+def test_contains(sample_data):
+    assert 3 in sample_data
+    assert 6 in sample_data
