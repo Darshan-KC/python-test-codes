42fdf39c4b4c68564095a852872449f8e71f19b2
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-08-17 22:14:02 +0545

    employee added

---
+++ pytest/class_test_2/employee.py
@@ -0,0 +1,5 @@
+# Write a class called Employee. The __init__() method should 
+# take in a first name, a last name, and an annual salary, and store each of these 
+# as attributes. Write a method called give_raise() that adds $5,000 to the 
+# annual salary by default but also accepts a different raise amount.
+
